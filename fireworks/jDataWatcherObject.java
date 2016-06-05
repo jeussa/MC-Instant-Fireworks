4396390e549481016bc3ab2fdb9d772f43acba18
jeussa <jeussa@users.noreply.github.com>
2016-06-05 12:55:05 +0200

    Update jDataWatcherObject.java

--- fireworks/jDataWatcherObject.java
+++ fireworks/jDataWatcherObject.java
@@ -1,14 +1,18 @@
-package fireworks;
+package com.jeussa.api.v4.bukkit.protocol;
 
 import org.bukkit.inventory.ItemStack;
 
+import com.jeussa.api.v4.bukkit.API;
+
 public class jDataWatcherObject<T> implements iNmsObject{
 	
-	public static final jDataWatcherObject<Byte> entity_ay=new jDataWatcherObject<>("ay", "Entity");
-	public static final jDataWatcherObject<Integer> entity_az=new jDataWatcherObject<>("az", "Entity");
-	public static final jDataWatcherObject<String> entity_aA=new jDataWatcherObject<>("aA", "Entity");
-	public static final jDataWatcherObject<Boolean> entity_aB=new jDataWatcherObject<>("aB", "Entity");
-	public static final jDataWatcherObject<Boolean> entity_aC=new jDataWatcherObject<>("aC", "Entity");
+	private static final boolean mc194=!ProtocolUtils.version.endsWith("R1");
+	
+	public static final jDataWatcherObject<Byte> entity_ay=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("ay"):("ax")), "Entity");
+	public static final jDataWatcherObject<Integer> entity_az=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("az"):("ay")), "Entity");
+	public static final jDataWatcherObject<String> entity_aA=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("aA"):("az")), "Entity");
+	public static final jDataWatcherObject<Boolean> entity_aB=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("aB"):("aA")), "Entity");
+	public static final jDataWatcherObject<Boolean> entity_aC=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("aC"):("aB")), "Entity");
 	public static final jDataWatcherObject<ItemStack> entityfireworks_FIREWORK_ITEM=new jDataWatcherObject<>("FIREWORK_ITEM", "EntityFireworks");
 	
 	
@@ -16,7 +20,7 @@ public class jDataWatcherObject<T> implements iNmsObject{
 	private final Object nms;
 	
 	private jDataWatcherObject(String fieldname, String classname){
-		this.nms=ProtocolUtils.refl_fieldGet0(fieldname, classname);
+		this.nms=jDataWatcherObject.util_getField(fieldname, classname);
 	}
 	
 	/**
@@ -25,5 +29,17 @@ public class jDataWatcherObject<T> implements iNmsObject{
 	@Override
 	public Object build(){
 		return this.nms;
+	}
+	
+	/**
+	 * Get
+	 */
+	private static Object util_getField(String fieldname, String classname){
+		try{
+			return API.reflectionUtil.field_get(fieldname, ProtocolUtils.getMinecraftClass(classname));
+		}catch(Exception e){
+			API.console.debug(e);
+			return null;
+		}
 	}
 }
