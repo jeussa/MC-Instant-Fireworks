5ff8ef24db0b330dda8f8c13411df694728775d7
jeussa <jeussa@users.noreply.github.com>
2016-06-11 09:49:43 +0200

    Update jDataWatcherObject.java

--- fireworks/jDataWatcherObject.java
+++ fireworks/jDataWatcherObject.java
@@ -4,14 +4,29 @@ import org.bukkit.inventory.ItemStack;
 
 public class jDataWatcherObject<T> implements iNmsObject{
 	
-	private static final boolean mc194=!ProtocolUtils.version.endsWith("R1");
+	private static int version;
 	
-	public static final jDataWatcherObject<Byte> entity_ay=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("ay"):("ax")), "Entity");
-	public static final jDataWatcherObject<Integer> entity_az=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("az"):("ay")), "Entity");
-	public static final jDataWatcherObject<String> entity_aA=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("aA"):("az")), "Entity");
-	public static final jDataWatcherObject<Boolean> entity_aB=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("aB"):("aA")), "Entity");
-	public static final jDataWatcherObject<Boolean> entity_aC=new jDataWatcherObject<>(((jDataWatcherObject.mc194)?("aC"):("aB")), "Entity");
-	public static final jDataWatcherObject<ItemStack> entityfireworks_FIREWORK_ITEM=new jDataWatcherObject<>("FIREWORK_ITEM", "EntityFireworks");
+	static{
+		String v=ProtocolUtils.version;
+		if(v.startsWith("v1_9")){
+			if(v.endsWith("R1"))jDataWatcherObject.version=0;
+			else jDataWatcherObject.version=1;
+		}
+		else if(v.startsWith("v1_10")){
+			jDataWatcherObject.version=2;
+		}
+	}
+	private static String v(String...values){
+		return values[jDataWatcherObject.version];
+	}
+	
+	public static final jDataWatcherObject<Byte> entity_byte=new jDataWatcherObject<>(v("ax", "ay", "aa"), "Entity");
+	public static final jDataWatcherObject<Integer> entity_int=new jDataWatcherObject<>(v("ay", "az", "az"), "Entity");
+	public static final jDataWatcherObject<String> entity_string=new jDataWatcherObject<>(v("az", "aA", "aA"), "Entity");
+	public static final jDataWatcherObject<Boolean> entity_boolean1=new jDataWatcherObject<>(v("aA", "aB", "aB"), "Entity");
+	public static final jDataWatcherObject<Boolean> entity_boolean2=new jDataWatcherObject<>(v("aB", "aC", "aC"), "Entity");
+	public static final jDataWatcherObject<Boolean> entity_boolean3=new jDataWatcherObject<>(v(null, null, "aD"), "Entity");
+	public static final jDataWatcherObject<ItemStack> entityfireworks_itemstack=new jDataWatcherObject<>("FIREWORK_ITEM", "EntityFireworks");
 	
 	
 	
